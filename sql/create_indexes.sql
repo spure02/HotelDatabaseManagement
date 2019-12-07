32e4ce346e96ae29278638f0e084db63a33f0f09
spure001@ucr.edu <spure001@ucr.edu>
2019-12-06 19:55:44 -0800

    made indexes, need to test

--- sql/create_indexes.sql
+++ sql/create_indexes.sql
@@ -1,11 +1,35 @@
-CREATE INDEX on_hand_test_nyc
-ON part_nyc
-(on_hand);
+CREATE INDEX hotel_index
+ON Hotel
+(hotelID);
 
-CREATE INDEX on_hand_test_sfo
-ON part_sfo
-(on_hand);
+CREATE INDEX staff_index
+ON Staff
+(staffID);
 
-CREATE INDEX supplier_test
-ON supplier
-(supplier_id);
+CREATE INDEX room_index
+ON Room
+(roomID);
+
+CREATE INDEX customer_index
+ON Customer
+(customerID);
+
+CREATE INDEX company_index
+ON MaintenanceCompany
+(cmpID);
+
+CREATE INDEX booking_index
+ON Booking
+(bookingDate);
+
+CREATE INDEX repair_index
+ON Repair
+(mCompany);
+
+CREATE INDEX request_index
+ON Request
+(managerID);
+
+CREATE INDEX assigned_index
+ON Assigned
+(staffID);
